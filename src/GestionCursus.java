e958acb640d49029a5f7332db5a31de42982c169
Anne <patesdegeek@gmail.com>
2020-05-01 16:28:03 +0200

    ajout methode classe GestionCursus

--- src/GestionCursus.java
+++ src/GestionCursus.java
@@ -1,2 +1,10 @@
 public class GestionCursus {
+    public static void main(String[] args) {
+        Cursus C = new Cursus();
+        System.out.println("------------  Récapitulatif  --------------");
+        C.afficheLesEtudiants();
+        C.classerParMoyenne();
+        System.out.println("--------------  Classement  ---------------");
+        C.afficheLesEtudiants();
+    }
 }
