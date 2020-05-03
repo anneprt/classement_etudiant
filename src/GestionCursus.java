90e5fbd920f501f81814fd9972f76c4dff838633
Anne <patesdegeek@gmail.com>
2020-05-03 15:19:59 +0200

    modification class GestionCursus

--- src/GestionCursus.java
+++ src/GestionCursus.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class GestionCursus {
     public static void main(String[] args) {
+        byte choix;
+        Scanner lectureClavier = new Scanner(System.in);
         Cursus C = new Cursus();
-        Etudiant E = new Etudiant();
-        System.out.println("------------  Récapitulatif  --------------");
-        C.afficheLesEtudiants();
-        C.classerParMoyenne();
-        System.out.println("--------------  Classement  ---------------");
-        C.afficheLesEtudiants();
+        do {
+            System.out.println("1. Ajoute un etudiant");
+            System.out.println("2. Affiche la liste des etudiants");
+            System.out.println("3. Pour sortir");
+            System.out.print("Votre choix : ");
+            choix = lectureClavier.nextByte();
+            switch (choix) {
+                case 1:
+                    C.ajouteUnEtudiant();
+                    break;
+                case 2:
+                    C.afficheLesEtudiants();
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("Cette option n'existe pas");
+            }
+        }
+        while (choix != 3);
     }
 }
